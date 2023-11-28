079e7576745e03e6541276b4a93e6755321ae15a
Aiden.choi <recovery@asleep.ai>
2023-11-28 11:41:23 +0900

    make installability

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,19 +2,56 @@ import {MetadataRoute} from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js App',
-        short_name: 'Next.js App',
-        description: 'Next.js App',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#fff',
-        theme_color: '#fff',
-        icons: [
+        "background_color": "#fff",
+        "description": "에이든, 루나, 에이버리의 유작. 여기에 남다",
+        "display": "standalone",
+        "display_override": ["fullscreen", "minimal-ui"],
+        "icons": [
             {
-                src: '/favicon.ico',
-                sizes: 'any',
-                type: 'image/x-icon',
+                "purpose": "any",
+                "sizes": "114x114",
+                "src": "/image/favicon/favicon-114x114.png",
+                "type": "image/png"
             },
+            {
+                "purpose": "any",
+                "sizes": "144x144",
+                "src": "/image/favicon/favicon-144x144.png",
+                "type": "image/png"
+            },
+            {
+                "purpose": "any",
+                "sizes": "512x512",
+                "src": "/image/favicon/favicon-512x512.png",
+                "type": "image/png"
+            },
+            {
+                "purpose": "any",
+                "sizes": "192x192",
+                "src": "/image/favicon/favicon-192x192.png",
+                "type": "image/png"
+            }
         ],
+        "name": "에이슬립피드",
+        "prefer_related_applications": false,
+        "screenshots": [
+            {
+                "form_factor": "wide",
+                "label": "Homescreen of Awesome App",
+                "sizes": "1080x1920",
+                "src": "/image/screenshot/1.jpg",
+                "type": "image/jpg"
+            },
+            {
+                "form_factor": "narrow",
+                "label": "Homescreen of Awesome App",
+                "sizes": "1080x1920",
+                "src": "/image/screenshot/1.jpg",
+                "type": "image/jpg"
+            }
+        ],
+        "short_name": "에이슬립피드",
+        "start_url": "/",
+        "theme_color": "#fff"
     }
 }
