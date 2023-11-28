92e61432539b7b371a5b44a2b9e218604d4ae860
Aiden.choi <recovery@asleep.ai>
2023-11-28 20:40:36 +0900

    implement web push

---
+++ app/subscribe.ts
@@ -0,0 +1,24 @@
+import {CONFIG} from "@/app/webPushConfig";
+import {saveSubscription} from "@/app/saveSubscription";
+
+export async function subscribe() {
+    await window?.Notification.requestPermission();
+
+    try {
+        const options = {
+            applicationServerKey: CONFIG.PUBLIC_KEY,
+            userVisibleOnly: true,
+        }
+
+        const swRegistration = await window.navigator.serviceWorker.getRegistration()
+        const subscription = await swRegistration?.pushManager.subscribe(options)
+
+        if (subscription) {
+            // await saveSubscription(subscription)
+        }
+
+        console.log({subscription})
+    } catch (err) {
+        console.error('Error', err)
+    }
+}
---
+++ app/webPushConfig.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+    PUBLIC_KEY:
+        'BBAkBx7Gsc4aIy10yELuAXcbSvyKOO5eBr16QqmmDl6qPo8k8ILY6_63KzuhMaoAMdxLOGdw_oMzWUmV8AHMt4U',
+    PRIVATE_KEY: 'FYz0wve1vRYw0wm6NDMWTDxUpVAStglD0-G5RtWVn_8',
+}
